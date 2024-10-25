f592e4db26d2124f1aa6485785aa76ac16c0a940
Aram <a01642781@tec.mx>
2024-10-25 01:46:30 -0600

    Mayor velocidad en proyectil y balones

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 350) / 25
+        speed.y = (y + 350) / 25
 
 
 def inside(xy):
@@ -74,7 +74,7 @@ def move():
         if not inside(target):
             return
 
-    ontimer(move, 50)
+    ontimer(move, 15)
 
 
 setup(420, 420, 370, 0)
